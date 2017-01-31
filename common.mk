1cc73bfee282b92501ad6c0f8f467e5b22fa5dc9
Rob Skillington <rob.skillington@gmail.com>
2017-01-31 14:07:40 -0500

    Ensure glide install does not execute when glide is available (#6)

--- common.mk
+++ common.mk
@@ -1,9 +1,16 @@
+
 install-vendor: install-glide
 	@echo Installing glide deps
 	glide --debug install
 
+HAS_GLIDE := $(glide -version 2>&1 > /dev/null)
+
 install-glide:
-	@which glide > /dev/null || go get -u github.com/Masterminds/glide && cd $(GOPATH)/src/github.com/Masterminds/glide && git checkout v0.12.3 && go install
+ifdef HAS_GLIDE
+	go get -u github.com/Masterminds/glide && cd $(GOPATH)/src/github.com/Masterminds/glide && git checkout v0.12.3 && go install
+endif
+	@glide -version > /dev/null || echo "Glide install failed"
 
 install-ci:
 	make install-vendor
+
