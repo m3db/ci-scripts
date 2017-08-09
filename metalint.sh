59751443083b1f9b76a5c5a25b0af4423917699e
Prateek Rungta <prateek86400@gmail.com>
2017-08-09 18:31:47 -0400

    add gometalinter script

---
+++ metalint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [[ $# -lt 2 ]]; then
+  echo "Usage: $0 <metalinter-config-file> <exclude-file> [<optional-args>, ...]"
+  exit 1
+fi
+
+config_file=$1
+shift
+exclude_file=$1
+shift
+additional_args=$*
+
+if [[ ! -f $exclude_file ]]; then
+  echo "exclude-file ($exclude_file) does not exist"
+  exit 1
+fi
+
+! gometalinter --config $config_file --vendor ${additional_args} ./... | egrep -v -f $exclude_file
