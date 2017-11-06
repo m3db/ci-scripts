93c94969f7218f4cc7d88abaecb3d3ec18729a51
Richard Artoul <rartoul@uber.com>
2017-11-06 17:16:27 -0500

    Automatically install maptime

--- common.mk
+++ common.mk
@@ -9,7 +9,12 @@ install-glide:
 install-ci:
 	make install-vendor
 
-install-metalinter:
+install-metalinter-internal:
 	@which gometalinter > /dev/null || (go get -u github.com/alecthomas/gometalinter && \
 		go install github.com/alecthomas/gometalinter && gometalinter --install)
 	@which gometalinter > /dev/null || (echo "gometalinter install failed" && exit 1)
+
+install-metalinter-plugins: install-metalinter-internal
+	@which maptime > /dev/null || (go get -u github.com/m3db/build-tools/linters/maptime && go install github.com/alecthomas/gometalinter)
+
+install-metalinter: install-metalinter-plugins
