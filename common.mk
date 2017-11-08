093154542fe3715e2ec599bb9760846405031bf0
Richard Artoul <rartoul@uber.com>
2017-11-08 13:50:56 -0500

    Move versions to variables

--- common.mk
+++ common.mk
@@ -1,3 +1,6 @@
+metalinter_version 	:= 0262fb20957a4c2d3bb7c834a6a125ae3884a2c6
+maptime_version 		:= a3f8910440bba296b90fd559b425009981c8bcb8
+
 install-vendor: install-glide
 	@echo Installing glide deps
 	glide --debug install
@@ -12,12 +15,12 @@ install-ci:
 install-metalinter:
 	@which gometalinter > /dev/null || (go get -u github.com/alecthomas/gometalinter && \
 		cd $(GOPATH)/src/github.com/alecthomas/gometalinter && \
-		git checkout 0262fb20957a4c2d3bb7c834a6a125ae3884a2c6 && \
+		git checkout $(metalinter_version) && \
 		go install && gometalinter --install)
 	@which gometalinter > /dev/null || (echo "gometalinter install failed" && exit 1)
 
 install-linter-maptime:
 	@which maptime > /dev/null || (go get -u github.com/m3db/build-tools/linters/maptime && \
 		cd $(GOPATH)/src/github.com/m3db/build-tools/linters/maptime && \
-		git checkout a3f8910440bba296b90fd559b425009981c8bcb8 && go install)
+		git checkout $(maptime_version) && go install)
 	@which maptime > /dev/null || (echo "maptime install failed" && exit 1)
