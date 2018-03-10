236dca13afe02f21077b35f2254f8a51b62a9a41
Prateek Rungta <prateek@users.noreply.github.com>
2018-03-10 15:19:28 -0500

    fix typo

--- go-build-ldflags.sh
+++ go-build-ldflags.sh
@@ -10,7 +10,7 @@ fi
 GIT_REVISION=$(git rev-parse --short HEAD)
 GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 BUILD_DATE=$(date '+%F-%T') # outputs something in this format 2017-08-21-18:58:45
-BUILD_TS_UNIX=$(date '+%s') # second ssince epoch
+BUILD_TS_UNIX=$(date '+%s') # second since epoch
 BASE_PACKAGE=${PROJECT_PACKAGE}/vendor/github.com/m3db/m3x/instrument
 
 LD_FLAGS="-X ${BASE_PACKAGE}.Revision=${GIT_REVISION} \
