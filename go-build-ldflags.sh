9c75a507cc45d2f09aa78f8a8a93ff344ce6775c
Prateek Rungta <prateek@users.noreply.github.com>
2018-03-13 18:28:12 -0400

    Add build time to ldflags (#34)

--- go-build-ldflags.sh
+++ go-build-ldflags.sh
@@ -10,11 +10,13 @@ fi
 GIT_REVISION=$(git rev-parse --short HEAD)
 GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 BUILD_DATE=$(date '+%F-%T') # outputs something in this format 2017-08-21-18:58:45
+BUILD_TS_UNIX=$(date '+%s') # second since epoch
 BASE_PACKAGE=${PROJECT_PACKAGE}/vendor/github.com/m3db/m3x/instrument
 
 LD_FLAGS="-X ${BASE_PACKAGE}.Revision=${GIT_REVISION} \
-  -X ${BASE_PACKAGE}.Branch=${GIT_BRANCH} \
-  -X ${BASE_PACKAGE}.BuildDate=${BUILD_DATE} \
+  -X ${BASE_PACKAGE}.Branch=${GIT_BRANCH}             \
+  -X ${BASE_PACKAGE}.BuildDate=${BUILD_DATE}          \
+  -X ${BASE_PACKAGE}.BuildTimeUnix=${BUILD_TS_UNIX}   \
   -X ${BASE_PACKAGE}.LogBuildInfoAtStartup=true"
 
 echo $LD_FLAGS
