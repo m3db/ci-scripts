259d0fe802b93e29c5aff2a45200a057cb10bed9
Prateek Rungta <rungta@uber.com>
2018-03-19 01:00:56 -0400

    Filter non-big unit tests when running separately

--- test-cover.sh
+++ test-cover.sh
@@ -35,7 +35,11 @@ TEST_EXIT=${PIPESTATUS[0]}
 echo "test-cover begin: concurrency 1, +big"
 for DIR in $DIRS; do
   if cat $DIR/*_test.go | grep "// +build" | grep "big" &>/dev/null; then
-    go test $TEST_FLAGS -tags big -coverprofile $PROFILE_BIG $DIR | tee $LOG
+    # extract only the tests marked "big"
+    BIG_TESTS=$(cat <(go test $DIR -tags big -list '.*' | grep -v '^ok' | grep -v 'no test files' ) \
+                    <(go test $DIR -list '.*' | grep -v '^ok' | grep -v 'no test files')            \
+                    | sort | uniq -u | paste -sd'|' -)
+    go test $TEST_FLAGS -tags big -run $BIG_TESTS -coverprofile $PROFILE_BIG $DIR | tee $LOG
     BIG_TEST_EXIT=${PIPESTATUS[0]}
     # Only set TEST_EXIT if its already zero to be prevent overwriting non-zero exit codes
     if [ "$TEST_EXIT" = "0" ]; then
