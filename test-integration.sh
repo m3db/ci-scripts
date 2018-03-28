d7590f704e2209dd3c22c4130148c106e41db692
Prateek Rungta <rungta@uber.com>
2018-03-27 21:52:10 -0400

    Re-work integration script to include coverage reporting

--- test-integration.sh
+++ test-integration.sh
@@ -6,21 +6,28 @@ set -e
 TAGS="integration"
 DIR="integration"
 INTEGRATION_TIMEOUT=${INTEGRATION_TIMEOUT:-10m}
+COVERFILE=${COVERFILE:-cover.out}
+COVERMODE=count
+
+echo "mode: ${COVERMODE}" > $COVERFILE
 
 # compile the integration test binary
-go test -test.c -test.tags=${TAGS} ./${DIR}
+go test -test.c -test.tags=${TAGS} -test.covermode ${COVERMODE} \
+  -test.coverpkg $(go list ./... | paste -sd, -) ./${DIR}
 
 # list the tests
-TESTS=$(./integration.test -test.v -test.short | grep RUN | tr -s " " | cut -d ' ' -f 3)
+TESTS=$(./integration.test -test.list '.*')
 
 # execute tests one by one for isolation
 for TEST in $TESTS; do
-  ./integration.test -test.v -test.run $TEST -test.timeout $INTEGRATION_TIMEOUT ./integration
+  ./integration.test -test.v -test.run $TEST -test.coverprofile temp_${COVERFILE} \
+  -test.timeout $INTEGRATION_TIMEOUT ./integration
   TEST_EXIT=$?
   if [ "$TEST_EXIT" != "0" ]; then
     echo "$TEST failed"
     exit $TEST_EXIT
   fi
+  cat temp_${COVERFILE} | grep -v '_mock.go' | grep -v "mode: " >> ${COVERFILE}
   sleep 0.1
 done
 
