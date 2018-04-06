f49913baf77d2e05de9235758dd143743b1f7d69
Xi Chen <xichen@uber.com>
2018-04-06 14:12:07 -0400

    Faster generated protobuf code

--- proto-gen.sh
+++ proto-gen.sh
@@ -8,6 +8,6 @@ for i in "$PROTO_SRC"/*; do
 
 	if ls $i/*.proto > /dev/null 2>&1; then
 			echo "generating from $i"
-			protoc -I$i --gofast_out=$i $i/*.proto
+			protoc -I$GOPATH/src/github.com/gogo/protobuf/gogoproto -I$PROTO_SRC --gogofaster_out=$PROTO_SRC $i/*.proto
 	fi
 done
