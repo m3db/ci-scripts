c5e55495f150ce61a5c031757bb4ee052cee1760
Rob Skillington <r@uber.com>
2018-05-11 19:10:55 -0400

    Add ability to exclude test subdirectory packages

--- variables.sh
+++ variables.sh
@@ -4,7 +4,18 @@
 export VENDOR_PATH=$PACKAGE/vendor
 export LICENSE_BIN=$GOPATH/src/$PACKAGE/.ci/uber-licence/bin/licence
 export GO15VENDOREXPERIMENT=1
-export SRC=$(find ./ -maxdepth 10 -not -path '*/.git*' -not -path '*/.ci*' -not -path '*/_*' -not -path '*/vendor/*' -type d)
+
+FIND_ROOT="./"
+if [ "$SRC_ROOT" != "" ]; then
+  FIND_ROOT=$SRC_ROOT
+fi
+
+FIND_EXCLUDE=""
+if [ "$SRC_EXCLUDE" != "" ]; then
+  FIND_EXCLUDE=" | grep -v $SRC_EXCLUDE"
+fi
+
+export SRC=$(find $FIND_ROOT -maxdepth 10 -not -path '*/.git*' -not -path '*/.ci*' -not -path '*/_*' -not -path '*/vendor/*' -type d $FIND_EXCLUDE)
 
 filter_cover_profile() {
   local input_profile_file=$1
