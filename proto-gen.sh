cea503b147a7fbb7213bba9c87f5e1744ebb69d4
Chao Wang <chaowang@uber.com>
2018-05-23 18:34:07 -0400

    Update proto-gen output path

--- proto-gen.sh
+++ proto-gen.sh
@@ -8,6 +8,6 @@ for i in "$PROTO_SRC"/*; do
 
 	if ls $i/*.proto > /dev/null 2>&1; then
 			echo "generating from $i"
-			protoc -I$i --gogofaster_out=plugins=grpc:$i --proto_path=$i:$GOPATH/src:$GOPATH/src/github.com/m3db/m3db/vendor $i/*.proto
+			protoc -I$i --gogofaster_out=plugins=grpc:$GOPATH/src --proto_path=$i:$GOPATH/src:$GOPATH/src/github.com/m3db/m3db/vendor $i/*.proto
 	fi
 done
