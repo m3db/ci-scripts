c4f5c4c875a9cdf95509171523e7ce7d644b0a96
Jerome Froelich <jeromefroe@uber.com>
2018-05-23 10:51:05 -0400

    Fix formatting

--- go-build-ldflags.sh
+++ go-build-ldflags.sh
@@ -14,8 +14,8 @@ BUILD_DATE=$(date '+%F-%T') # outputs something in this format 2017-08-21-18:58:
 BASE_PACKAGE=${PROJECT_PACKAGE}/vendor/github.com/m3db/m3x/instrument
 
 LD_FLAGS="-X ${BASE_PACKAGE}.Revision=${GIT_REVISION} \
--X ${BASE_PACKAGE}.Branch=${GIT_BRANCH} \
--X ${BASE_PACKAGE}.Version=${GIT_VERSION} \
+-X ${BASE_PACKAGE}.Branch=${GIT_BRANCH}    \
+-X ${BASE_PACKAGE}.Version=${GIT_VERSION}  \
 -X ${BASE_PACKAGE}.BuildDate=${BUILD_DATE} \
 -X ${BASE_PACKAGE}.LogBuildInfoAtStartup=true"
 
