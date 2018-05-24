b26f57c86194def4df14c3834b6cf6d6d9b957eb
Paul Schooss <pschooss@uber.com>
2018-05-24 16:22:02 -0400

    Testing which command fails

--- common.mk
+++ common.mk
@@ -28,8 +28,8 @@ install-glide:
 # compesate for the lack of. This conditional tests an environmental variable
 # injected into SEMAPHORE instances, https://semaphoreci.com/docs/available-environment-variables.html
 prep-semaphore:
-	@df -T | grep "/$$" | grep ext || (echo "Root filesystem isn't ext and most likey not compatiable with swap files. Skipping swap file setup")
-	$(SEMAPHORE) && (sudo swapoff -a && sudo dd if=/dev/zero of=/swapfile bs=1M count=8192 && sudo mkswap /swapfile && sudo chmod 0600 /swapfile && sudo swapon /swapfile)
+	$(SEMAPHORE) && (df -T | grep "/$$" | grep ext && \
+		sudo swapoff -a && sudo dd if=/dev/zero of=/swapfile bs=1M count=8192 && sudo mkswap /swapfile && sudo chmod 0600 /swapfile && sudo swapon /swapfile)
 
 install-ci:
 	make prep-semaphore # test to see if running on SEMAPHORE instance
