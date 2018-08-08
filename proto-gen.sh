5398b72e2898f06367fe04947354b1389d4f75fe
Justin Chan <juchan@uber.com>
2018-08-08 18:44:30 -0400

    Update m3db ref

--- proto-gen.sh
+++ proto-gen.sh
@@ -8,6 +8,6 @@ for i in "$PROTO_SRC"/*; do
 
 	if ls $i/*.proto > /dev/null 2>&1; then
 			echo "generating from $i"
-			protoc --gogofaster_out=plugins=grpc:$GOPATH/src --proto_path=$GOPATH/src:$GOPATH/src/github.com/m3db/m3db/vendor $i/*.proto
+			protoc --gogofaster_out=plugins=grpc:$GOPATH/src --proto_path=$GOPATH/src:$GOPATH/src/github.com/m3db/m3/vendor $i/*.proto
 	fi
 done
