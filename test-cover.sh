f6f80de546f7b81124d1c946c612592fc7c75c60
Matt Schallert <mattschallert@gmail.com>
2019-08-20 15:43:53 -0400

    [cover] deprecate use of gotestcover
    The current approach does not support Go modules. Also the author has
    deprecated the project: https://github.com/pierrre/gotestcover.

--- test-cover.sh
+++ test-cover.sh
@@ -8,9 +8,7 @@ EXCLUDE_FILE=${2:-.excludecoverage}
 LOG=${3:-test.log}
 CI_DIR=${4:-.}
 
-rm $TARGET &>/dev/null || true
-echo "mode: atomic" > $TARGET
-echo "" > $LOG
+rm -f "$TARGET" &>/dev/null || true
 
 DIRS=""
 for DIR in $SRC;
@@ -21,7 +19,7 @@ do
 done
 
 # defaults to all DIRS if the split vars are unset
-pick_subset "$DIRS" TESTS $SPLIT_IDX $TOTAL_SPLITS
+pick_subset "$DIRS" TESTS "$SPLIT_IDX" "$TOTAL_SPLITS"
 
 if [ "$NPROC" = "" ]; then
   NPROC=$(getconf _NPROCESSORS_ONLN)
@@ -31,13 +29,14 @@ echo "test-cover begin: concurrency $NPROC"
 
 PROFILE_REG="profile_reg.tmp"
 
-TEST_FLAGS="-v -race -timeout 5m -covermode atomic"
-go run $CI_DIR/.ci/gotestcover/gotestcover.go $TEST_FLAGS -coverprofile $PROFILE_REG -parallelpackages $NPROC $TESTS | tee $LOG
-TEST_EXIT=${PIPESTATUS[0]}
+echo 'mode: atomic' > "$TARGET"
+echo "" > "$LOG"
+go list ./... | xargs -P $NPROC -n1 -I{} sh -c "go test -v -race -timeout 5m -covermode=atomic -coverprofile=${PROFILE_REG} {} && tail -n +2 $PROFILE_REG >> $TARGET"
+TEST_EXIT=$?
 
-filter_cover_profile $PROFILE_REG $TARGET $EXCLUDE_FILE
+filter_cover_profile $PROFILE_REG "$TARGET" "$EXCLUDE_FILE"
 
 find . -not -path '*/vendor/*' | grep \\.tmp$ | xargs -I{} rm {}
 echo "test-cover result: $TEST_EXIT"
 
-exit $TEST_EXIT
+exit "$TEST_EXIT"
