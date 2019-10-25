b14fe17eca98061294379c9498a52505b9f50e36
Matt Schallert <mattschallert@gmail.com>
2019-10-25 13:42:32 -0400

    Revert "[cover] deprecate use of gotestcover (#89)"
    This reverts commit 323f860b6a669b26f8f4e0ca5f3ee5c4f8f6e94d.
    
    I believe this change has been causing corruption in our coverage
    reports due to races writing out results. Reverting until we have a
    better solution.

--- test-cover.sh
+++ test-cover.sh
@@ -8,7 +8,9 @@ EXCLUDE_FILE=${2:-.excludecoverage}
 LOG=${3:-test.log}
 CI_DIR=${4:-.}
 
-rm -f "$TARGET" &>/dev/null || true
+rm $TARGET &>/dev/null || true
+echo "mode: atomic" > $TARGET
+echo "" > $LOG
 
 DIRS=""
 for DIR in $SRC;
@@ -19,7 +21,7 @@ do
 done
 
 # defaults to all DIRS if the split vars are unset
-pick_subset "$DIRS" TESTS "$SPLIT_IDX" "$TOTAL_SPLITS"
+pick_subset "$DIRS" TESTS $SPLIT_IDX $TOTAL_SPLITS
 
 if [ "$NPROC" = "" ]; then
   NPROC=$(getconf _NPROCESSORS_ONLN)
@@ -29,14 +31,13 @@ echo "test-cover begin: concurrency $NPROC"
 
 PROFILE_REG="profile_reg.tmp"
 
-echo 'mode: atomic' > "$TARGET"
-echo "" > "$LOG"
-<<<"$TESTS" xargs -P $NPROC -n1 -I{} sh -c "go test -v -race -timeout 5m -covermode=atomic -coverprofile=${PROFILE_REG} {} && tail -n +2 $PROFILE_REG >> $TARGET"
-TEST_EXIT=$?
+TEST_FLAGS="-v -race -timeout 5m -covermode atomic"
+go run $CI_DIR/.ci/gotestcover/gotestcover.go $TEST_FLAGS -coverprofile $PROFILE_REG -parallelpackages $NPROC $TESTS | tee $LOG
+TEST_EXIT=${PIPESTATUS[0]}
 
-filter_cover_profile $PROFILE_REG "$TARGET" "$EXCLUDE_FILE"
+filter_cover_profile $PROFILE_REG $TARGET $EXCLUDE_FILE
 
 find . -not -path '*/vendor/*' | grep \\.tmp$ | xargs -I{} rm {}
 echo "test-cover result: $TEST_EXIT"
 
-exit "$TEST_EXIT"
+exit $TEST_EXIT
