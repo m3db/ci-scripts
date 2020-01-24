1c87fdbb71a0a3bfbd0c15945fe58a810a1062ef
Matt Schallert <matt@chronosphere.io>
2020-01-24 12:24:28 -0500

    [cover] Output failed packages at end of tests (#96)

--- test-cover.sh
+++ test-cover.sh
@@ -3,6 +3,12 @@ set -e
 
 source "$(dirname $0)/variables.sh"
 
+function bk_log() {
+  if [[ "${BUILDKITE}" == "true" ]]; then
+    echo "--- $1"
+  fi
+}
+
 TARGET=${1:-profile.cov}
 EXCLUDE_FILE=${2:-.excludecoverage}
 LOG=${3:-test.log}
@@ -30,14 +36,13 @@ fi
 # spaces.
 NPROC=${NPROC// /}
 
+bk_log ":golang: Running tests and coverage"
 echo "test-cover begin: concurrency $NPROC"
 
 PROFILE_REG="profile_reg.tmp"
 
-# Ideally we'd use $(mktemp -d), but xargs -I{} limits resulting strings to 255
-# bytes and some systems (such as MacOS) generate insanely long tmpdir names.
-# This makes sure we have better control of the arg length.
-COVERTMP="covertmp"
+export COVERTMP
+COVERTMP="$(mktemp -d)"
 rm -rf "$COVERTMP"
 mkdir -p "$COVERTMP"
 
@@ -52,22 +57,33 @@ echo "" > "$LOG"
 
 # In parallel, write each package's coverage information to a package-specific
 # file. Sanitize each package path to a file-friendly name.
-#
-# GNU xargs only takes newlines as separator if using -I. Can get around this
-# with `-d '\n'`, but that doesn't work on BSD (+MacOS) xargs. Replacing spaces
-# with newlines is cross-platform friendly.
-echo "$TESTS" | tr ' ' '\n' | xargs -P "$NPROC" -n1 -I "{}" sh -c "DIR=\$(echo {} | sed 's@[/|\.]@_@g'); go test -v -race -timeout 5m -covermode=atomic -coverprofile=${COVERTMP}/\$DIR {}"
-
+set +e
+<<<"$TESTS" xargs -P "$NPROC" -n1 .ci/process-cover-package.sh
 TEST_EXIT=$?
+set -e
+
+FAILED_PKGS=""
 
 # Combine all per-package cover results into one larger result.
-find "$COVERTMP/" -type f | while read -r F; do
-  tail -n +2 "$F" >> "$PROFILE_REG"
-done
+while read -r F; do
+  if [[ "$(head -c 6 "$F")" == "FAILED" ]]; then
+    FAILED_PKGS="${FAILED_PKGS} $(awk '{print $2}' "$F")"
+  else
+    tail -n +2 "$F" >> "$PROFILE_REG"
+  fi
+# NB(schallert): see https://github.com/koalaman/shellcheck/wiki/SC2031 for why
+# find results are passed at end of loop to preserve FAILED_PKGS
+done < <(find "$COVERTMP/" -type f)
 
 filter_cover_profile $PROFILE_REG "$TARGET" "$EXCLUDE_FILE"
 
 find . -not -path '*/vendor/*' | grep \\.tmp$ | xargs -I{} rm {}
+
+if [[ -n "$FAILED_PKGS" ]]; then
+  bk_log ":bk-status-failed: encountered package failure(s)"
+  echo "packages with failures: [${FAILED_PKGS}]"
+fi
+
 echo "test-cover result: $TEST_EXIT"
 
 exit $TEST_EXIT
