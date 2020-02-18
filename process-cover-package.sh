d4ff6fbc10c8a2b33b0eb7ce94a3b9aa1dc647f6
Matt Schallert <matt@chronosphere.io>
2020-02-18 13:29:35 -0500

    [test-cover] Support Go build tags via env var

--- process-cover-package.sh
+++ process-cover-package.sh
@@ -6,7 +6,14 @@ set -eo pipefail
 DIR=$(<<<"$1" sed 's@[/|\.]@_@g')
 
 OUT="${COVERTMP}/${DIR}"
-if ! go test -v -race -timeout 5m -covermode=atomic -coverprofile="$OUT" "$1"; then
+
+# If using any manual build tags, propagate them to `go test`.
+TAGS=()
+if [[ -n "$GO_BUILD_TAGS" ]]; then
+  TAGS=("-tags" "${GO_BUILD_TAGS}")
+fi
+
+if ! go test "${TAGS[@]}" -v -race -timeout 5m -covermode=atomic -coverprofile="$OUT" "$1"; then
   echo "FAILED $1" > "$OUT"
   exit 1
 fi
