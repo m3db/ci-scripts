cfabd4878a1f36541d9551117ad65f1294046609
Matt Schallert <mattschallert@gmail.com>
2021-02-02 12:20:51 -0500

    Update process-cover-package.sh

--- process-cover-package.sh
+++ process-cover-package.sh
@@ -13,7 +13,7 @@ if [[ -n "$GO_BUILD_TAGS" ]]; then
   TAGS=("-tags" "${GO_BUILD_TAGS}")
 fi
 
-if ! go test "${TAGS[@]}" -v -race -timeout 10m -covermode=atomic -coverprofile="$OUT" "$1"; then
+if ! go test "${TAGS[@]}" -v -race -timeout "${TEST_TIMEOUT:-10m}" -covermode=atomic -coverprofile="$OUT" "$1"; then
   echo "FAILED $1" > "$OUT"
   exit 1
 fi
