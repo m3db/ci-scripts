51617b2c8e9bc176a500bb893ef680c6227883ca
Aaron Harlap <aaron@chronosphere.io>
2021-08-23 09:37:37 -0400

    Support specifying json output for tests.

--- process-cover-package.sh
+++ process-cover-package.sh
@@ -13,7 +13,12 @@ if [[ -n "$GO_BUILD_TAGS" ]]; then
   TAGS=("-tags" "${GO_BUILD_TAGS}")
 fi
 
-if ! go test "${TAGS[@]}" -v -race -timeout "${TEST_TIMEOUT:-10m}" -covermode=atomic -coverprofile="$OUT" "$1"; then
+OUTPUT_FORMAT_ARG=""
+if [[ $GO_TEST_OUTPUT_FORMAT == "json" ]]; then
+    OUTPUT_FORMAT_ARG="-json"
+fi
+
+if ! go test "${TAGS[@]}" -v $OUTPUT_FORMAT_ARG -race -timeout "${TEST_TIMEOUT:-10m}" -covermode=atomic -coverprofile="$OUT" "$1"; then
   echo "FAILED $1" > "$OUT"
   exit 1
 fi
